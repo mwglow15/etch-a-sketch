c9a5ab552507718b38eccf5a82eeec840fd26e7b
mattwg <mattwglowacki@gmail.com>
2022-05-18 12:34:53 -0400

    Create tile grid with tile width calc from board size

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const board = document.querySelector('.board');
 
 const tile = document.createElement('div');
 tile.classList.add('tile');
-console.log(board.style.width);
-tile.style.width = 10%
+boardWidth = board.clientWidth - 2*parseInt(getComputedStyle(board).padding);
+tile.style.width = boardWidth / 16 + "px";
 tile.style.height = tile.style.width;
 
 for (i=0; i<16*16 ; i++) {
