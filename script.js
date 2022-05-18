294b6d5f7c0ae5780a30a059c8d6d501c43d66b6
mattwg <mattwglowacki@gmail.com>
2022-05-18 12:59:17 -0400

    Make tile numbers a variable

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+let squareSize = 16;
 const board = document.querySelector('.board');
 
 const tile = document.createElement('div');
+
 tile.classList.add('tile');
 boardWidth = board.clientWidth - 2*parseInt(getComputedStyle(board).padding);
-tile.style.width = boardWidth / 16 + "px";
+tile.style.width = boardWidth / squareSize + "px";
 tile.style.height = tile.style.width;
 
-for (i=0; i<16*16 ; i++) {
+for (i=0; i<Math.pow(squareSize) ; i++) {
     board.appendChild(tile.cloneNode(true));
 }
