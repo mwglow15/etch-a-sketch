3815fa29b351e3eacaa8ff0d7581e6eec4fe5809
mattwg <mattwglowacki@gmail.com>
2022-05-18 13:23:46 -0400

    Add eventlistener to draw mouse path

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ const tile = document.createElement('div');
 
 tile.classList.add('tile');
 boardWidth = board.clientWidth - 2*parseInt(getComputedStyle(board).padding);
-tile.style.width = boardWidth / squareSize + "px";
+
+tile.style.width = (boardWidth / 16) + "px";
 tile.style.height = tile.style.width;
 
-for (i=0; i<Math.pow(squareSize) ; i++) {
+for (i=0; i<16*16 ; i++) {
     board.appendChild(tile.cloneNode(true));
 }
+
+
+function addDrawn(e) {
+    this.classList.add('drawn');
+}
+const tiles = document.querySelectorAll(".tile");
+
+tiles.forEach(elem => elem.addEventListener('mouseenter', addDrawn));
